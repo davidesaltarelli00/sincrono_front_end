9f5c9bc6dc7bbd6e48cef864462ce1ad3807ba0d
davidesaltarelli00 <133639028+davidesaltarelli00@users.noreply.github.com>
2023-05-22 14:57:26 +0200

    prova

---
+++ src/app/components/anagrafica/anagrafica.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anagrafica',
+  templateUrl: './anagrafica.component.html',
+  styleUrls: ['./anagrafica.component.scss']
+})
+export class AnagraficaComponent {
+
+}
