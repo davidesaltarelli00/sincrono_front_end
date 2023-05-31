74d5db6d195418cc4708ad8f0f22ee22ba8a9f69
Ruamez <ruamepnn@gmail.com>
2023-05-31 16:40:25 +0200

    31/5/23 Aggiunti bottoni per navigazione+ scss

--- src/app/components/commessa/dettaglio-commessa/dettaglio-commessa.component.ts
+++ src/app/components/commessa/dettaglio-commessa/dettaglio-commessa.component.ts
@@ -1,8 +1,8 @@
 import { CommessaService } from './../commessa-service.component';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AbstractControl, FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-dettaglio-commessa',
   templateUrl: './dettaglio-commessa.component.html',
@@ -13,12 +13,31 @@ export class DettaglioCommessaComponent implements OnInit {
   data: any;
 
 
-  constructor(private CommessaService: CommessaService, private router: ActivatedRoute, private formBuilder: FormBuilder, private router2: Router) {}
+  constructor(private CommessaService: CommessaService, private router: ActivatedRoute, private formBuilder: FormBuilder, private router2: Router, private location: Location) { }
 
-ngOnInit(): void {
-  this.CommessaService.detail(this.id).subscribe((resp: any) => {
-    console.log(resp);
-    this.data = (resp as any)["commessa"];
-  })
-}
+  reloadPage(): void {
+    this.location.go(this.location.path());
+    location.reload();
+  }
+  ngOnInit(): void {
+    this.CommessaService.detail(this.id).subscribe((resp: any) => {
+      console.log(resp);
+      this.data = (resp as any)["commessa"];
+    })
+  }
+
+  delete(id: number) {
+    this.CommessaService.delete(id).subscribe(
+      () => {
+        console.log('Commessa deleted');
+        this.reloadPage();
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+
+
 }
