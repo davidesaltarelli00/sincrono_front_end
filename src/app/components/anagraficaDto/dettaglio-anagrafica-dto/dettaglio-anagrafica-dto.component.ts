8da0074841ed78cabf2d54514be60bd7645cfa3e
Ruamez <ruamepnn@gmail.com>
2023-06-19 15:01:03 +0200

    19/6 fixed storico Commesse

--- src/app/components/anagraficaDto/dettaglio-anagrafica-dto/dettaglio-anagrafica-dto.component.ts
+++ src/app/components/anagraficaDto/dettaglio-anagrafica-dto/dettaglio-anagrafica-dto.component.ts
@@ -98,6 +98,6 @@ export class DettaglioAnagraficaDtoComponent {
     this.router.navigate(['/storico-contratti', idAnagrafica]);
   }
   getStoricoCommessa(idAnagrafica:any) {
-    this.router.navigate(['/storico-commesse', idAnagrafica]);
+    this.router.navigate(['/storico-commesse-anagrafica', idAnagrafica]);
   }
 }
