97b64adffa288d103bc0c26050efb3d7a7fbc62b
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-07-05 12:21:32 +0200

    prove layout e nascosta sidebar quando ci si trova nella pagina di login

--- src/app/components/login/login/login.component.ts
+++ src/app/components/login/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  showSidebar: boolean = false;
+  constructor(private router: Router) {
+    const currentUrl = this.router.url;
+    const isLoginPage = currentUrl === '/login'; // Aggiusta l'URL in base alla tua configurazione di routing
 
+    if (isLoginPage) {
+      document.body.classList.add('login-page');
+    } else {
+      document.body.classList.remove('login-page');
+    }
+  }
 }
