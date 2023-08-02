d6b71436f2800d0e54ad77ac93e9cab93fd37406
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-08-02 16:14:05 +0200

    quinto tentativo di rifare il form, i dati delle commesse finalmente sembrano arrivare correttamente ma nel momento in cui le invio al backend l array si svuota

--- src/app/components/anagraficaDto/modifica-anagrafica-dto/modifica-anagrafica-dto.component.ts
+++ src/app/components/anagraficaDto/modifica-anagrafica-dto/modifica-anagrafica-dto.component.ts
@@ -16,7 +16,7 @@ import { AnagraficaDtoService } from './../anagraficaDto-service';
 })
 export class ModificaAnagraficaDtoComponent implements OnInit {
   utenti: any = [];
-  data: any = [];
+  data: any;
   id = this.activatedRouter.snapshot.params['id'];
   submitted = false;
   errore = false;
@@ -27,12 +27,12 @@ export class ModificaAnagraficaDtoComponent implements OnInit {
   tipiContratti: any = [];
   livelliContratti: any = [];
   tipiAziende: any = [];
-  contrattiNazionali: any = [];
+  ccnl: any = [];
   ruoli: any = [];
   currentStep = 1;
   anagraficaDto: FormGroup;
-  commesse!: FormArray;
-  formsDuplicati: any;
+  commessePresenti = false;
+  elencoCommesse: any[] = [];
 
   constructor(
     private anagraficaDtoService: AnagraficaDtoService,
@@ -41,310 +41,192 @@ export class ModificaAnagraficaDtoComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {
-    this.anagraficaDto = this.formBuilder.group({
-      anagrafica: this.formBuilder.group({
-        attivo: new FormControl(''),
-        aziendaTipo: new FormControl(''),
-        nome: new FormControl('', Validators.required),
-        cognome: new FormControl('', Validators.required),
-        codiceFiscale: new FormControl('', Validators.required),
-        cellularePrivato: new FormControl(''),
-        cellulareAziendale: new FormControl(''),
-        mailPrivata: new FormControl(''),
-        mailPec: new FormControl(''),
-        mailAziendale: new FormControl('', Validators.required),
-        titoliDiStudio: new FormControl(''),
-        altriTitoli: new FormControl(''),
-        comuneDiNascita: new FormControl(''),
-        residenza: new FormControl(''),
-        domicilio: new FormControl(''),
-        dataDiNascita: new FormControl(''),
-        coniugato: new FormControl(''),
-        figliACarico: new FormControl(''),
-      }),
-      // commesse: this.formBuilder.array([
-      //   this.formBuilder.group({
-      //     id:new FormControl(''),
-      //     cliente: new FormControl(''),
-      //     clienteFinale: new FormControl(''),
-      //     titoloPosizione: new FormControl(''),
-      //     distacco: new FormControl(''),
-      //     costoMese: new FormControl(''),
-      //     dataInizio: new FormControl(''),
-      //     dataFine: new FormControl(''),
-      //     tariffaGiornaliera: new FormControl(''),
-      //     nominativo: new FormControl(''),
-      //     azienda: new FormControl(''),
-      //     aziendaDiFatturazioneInterna: new FormControl(''),
-      //     stato: new FormControl(''),
-      //     attesaLavori: new FormControl(''),
-      //   }),
-      // ]),
-
-      commesse: this.formBuilder.array([this.creaFormCommessa()]),
-
-      contratto: this.formBuilder.group({
-        attivo: new FormControl(''),
-        aziendaDiFatturazioneInterna: new FormControl(''),
-        tipoAzienda: new FormGroup({
-          id: new FormControl(''),
-        }),
-        tipoContratto: new FormGroup({
-          id: new FormControl(''),
-        }),
-        livelloContratto: new FormGroup({
-          id: new FormControl(''),
-        }),
-        contrattoNazionale: new FormGroup({
-          //ccnl
-          id: new FormControl(''),
-        }),
-        qualifica: new FormControl(''),
-        sedeAssunzione: new FormControl(''),
-        dataAssunzione: new FormControl(''),
-        dataInizioProva: new FormControl(''),
-        dataFineProva: new FormControl(''),
-        dataFineRapporto: new FormControl(''),
-        mesiDurata: new FormControl(''),
-        livelloIniziale: new FormControl(''),
-        // livelloAttuale: new FormControl(''),
-        // livelloFinale: new FormControl(''),
-        dimissioni: new FormControl(''),
-        partTime: new FormControl(''),
-        partTimeA: new FormControl(''),
-        retribuzioneMensileLorda: new FormControl(''),
-        superminimoMensile: new FormControl(''),
-        ralAnnua: new FormControl(''),
-        superminimoRal: new FormControl(''),
-        diariaMese: new FormControl(''),
-        diariaGg: new FormControl(''),
-        ticket: new FormControl(''),
-        valoreTicket: new FormControl(''),
-        categoriaProtetta: new FormControl(''),
-        tutor: new FormControl(''),
-        pfi: new FormControl(''),
-        corsoSicurezza: new FormControl(''),
-        motivazioneFineRapporto: new FormControl(''),
-        scattiAnzianita: new FormControl(''),
-        pc: new FormControl(''),
-        tariffaPartitaIva: new FormControl(''),
-        canaleReclutamento: new FormControl(''),
-      }),
-      ruolo: this.formBuilder.group({
-        id: new FormControl('', Validators.required),
-      }),
-    });
-
-    this.commesse = this.anagraficaDto.get('commesse') as FormArray;
-  }
-
-  ngOnInit(): void {
-    this.anagraficaDtoService
-      .detailAnagraficaDto(this.activatedRouter.snapshot.params['id'])
-      .subscribe((resp: any) => {
-        this.data = (resp as any)['anagraficaDto'];
-        // console.log(this.data);
-        this.anagraficaDto.patchValue(this.data);
-      });
-
     this.caricaListaUtenti();
     this.caricaTipoContratto();
     this.caricaLivelloContratto();
     this.caricaTipoAzienda();
     this.caricaContrattoNazionale();
+    this.caricaDati();
+    this.caricaRuoli();
+    this.anagraficaDto = this.formBuilder.group({
+      anagrafica: this.formBuilder.group({
+        id: [this.id], // Per il campo 'id' puoi usare il valore di 'this.id' se hai già inizializzato la variabile
+        attivo: [''], // Esempio di campo richiesto
+        aziendaTipo: [''],
+        nome: ['', Validators.required],
+        cognome: ['', Validators.required],
+        codiceFiscale: ['', Validators.required],
+        comuneDiNascita: [''],
+        dataDiNascita: [''],
+        residenza: [''],
+        domicilio: [''],
+        cellularePrivato: ['', Validators.required],
+        cellulareAziendale: ['', Validators.required],
+        mailPrivata: ['', Validators.required],
+        mailAziendale: ['', Validators.required],
+        mailPec: [''],
+        titoloDiStudio: [''],
+        altriTitoli: [''],
+        coniugato: [''],
+        figliAcarico: [''],
+      }),
+      commesse: this.formBuilder.array([]),
+      contratto: this.formBuilder.group({
+        tipoContratto: this.formBuilder.group({
+          id: [''],
+          descrizione: [''],
+        }),
+        livelloContratto: this.formBuilder.group({
+          id: [''],
+          ccnl: [''],
+          descrizione: [''],
+          minimiRet23: [''],
+        }),
+        tipoAzienda: this.formBuilder.group({
+          id: [''],
+          descrizione: [''],
+        }),
+        ccnl: this.formBuilder.group({
+          id: [''],
+          descrizione: [''],
+        }),
+        attivo: [''],
+        sedeAssunzione: [''],
+        qualifica: [''],
+        dataAssunzione: [''],
+        dataInizioProva: [''],
+        dataFineProva: [''],
+        dataFineRapporto: [''],
+        mesiDurata: [''],
+        livelloAttuale: [''],
+        livelloFinale: [''],
+        dimissioni: [''],
+        partTime: [''],
+        partTimeA: [''],
+        retribuzioneMensileLorda: [''],
+        superminimoMensile: [''],
+        ralAnnua: [''],
+        superminimoRal: [''],
+        diariaMese: [''],
+        diariaGg: [''],
+        ticket: [''],
+        valoreTicket: [''],
+        categoriaProtetta: [''],
+        tutor: [''],
+        pfi: [''],
+        assicurazioneObbligatoria: [''],
+        corsoSicurezza: [''],
+        motivazioneFineRapporto: [''],
+        pc: [''],
+        scattiAnzianita: [''],
+        tariffaPartitaIva: [''],
+        canaleReclutamento: [''],
+      }),
+      ruolo: this.formBuilder.group({
+        id: [''],
+        nome: [''],
+        descrizione: [''],
+      }),
+    });
+  }
+  getCommessas(): FormArray {
+    return this.anagraficaDto.get('commesse') as FormArray;
+  }
 
+  ngOnInit(): void {
+    this.caricaListaUtenti();
+    this.caricaTipoContratto();
+    this.caricaLivelloContratto();
+    this.caricaTipoAzienda();
+    this.caricaContrattoNazionale();
+    this.caricaDati();
     this.caricaRuoli();
   }
 
-  caricaTipoContratto() {
-    this.anagraficaDtoService.getTipoContratto().subscribe((result: any) => {
-      // console.log(result);
-      this.tipiContratti = (result as any)['list'];
-    });
-  }
-  caricaLivelloContratto() {
-    this.anagraficaDtoService.getLivelloContratto().subscribe((result: any) => {
-      // console.log(result);
-      this.livelliContratti = (result as any)['list'];
-    });
-  }
-  caricaTipoAzienda() {
-    this.anagraficaDtoService.getTipoAzienda().subscribe((result: any) => {
-      // console.log(result);
-      this.tipiAziende = (result as any)['list'];
-    });
-  }
+  // caricaDati(): void {
+  //   this.anagraficaDtoService
+  //     .detailAnagraficaDto(this.activatedRouter.snapshot.params['id'])
+  //     .subscribe((resp: any) => {
+  //       this.data = (resp as any)['anagraficaDto'];
+  //       console.log(JSON.stringify(resp));
+  //       this.anagraficaDto.patchValue(this.data);
 
-  caricaContrattoNazionale() {
+  //       // Assegna l'array delle commesse a elencoCommesse
+  //       this.elencoCommesse = this.data?.commessa ? [this.data.commessa] : [];
+  //       console.log(this.elencoCommesse)
+  //     });
+  // }
+  caricaDati(): void {
     this.anagraficaDtoService
-      .getContrattoNazionale()
-      .subscribe((result: any) => {
-        // console.log(result);
-        this.contrattiNazionali = (result as any)['list'];
+      .detailAnagraficaDto(this.activatedRouter.snapshot.params['id'])
+      .subscribe((resp: any) => {
+        this.data = (resp as any)['anagraficaDto'];
+        console.log(JSON.stringify(resp));
+        this.elencoCommesse = [this.data.commessa]; // Assegna i dati delle commesse all'array elencoCommesse
+        this.anagraficaDto.patchValue(this.data);
+        console.log(
+          'Elenco commesse presenti: ' + JSON.stringify(this.elencoCommesse)
+        );
       });
   }
-  caricaListaUtenti() {
-    this.anagraficaDtoService.getListaUtenti().subscribe((result: any) => {
-      // console.log(result);
-      this.utenti = (result as any)['list'];
+
+  getCommessaFormGroup(index: number): FormGroup {
+    return (this.anagraficaDto.get('commesse') as FormArray).controls[
+      index
+    ] as FormGroup;
+  }
+
+  addCommessa(): void {
+    const commesseArray = this.anagraficaDto.get('commesse') as FormArray;
+    commesseArray.push(this.creaFormCommessa());
+  }
+
+  removeCommessa(index: number): void {
+    const commesseArray = this.anagraficaDto.get('commesse') as FormArray;
+    commesseArray.removeAt(index);
+  }
+
+  creaFormCommessa(): FormGroup {
+    return this.formBuilder.group({
+      id: new FormControl(''),
+      cliente: new FormControl(''),
+      clienteFinale: new FormControl(''),
+      titoloPosizione: new FormControl(''),
+      distacco: new FormControl(''),
+      dataInizio: new FormControl(''),
+      dataFine: new FormControl(''),
+      costoMese: new FormControl(''),
+      tariffaGiornaliera: new FormControl(''),
+      nominativo: new FormControl(''),
+      azienda: new FormControl(''),
+      aziendaDiFatturazioneInterna: new FormControl(''),
+      stato: new FormControl(false),
+      attesaLavori: new FormControl(false),
     });
   }
+
   aggiorna() {
-    // const removeEmpty = (obj: any) => {
-    //   Object.keys(obj).forEach((key) => {
-    //     if (obj[key] && typeof obj[key] === 'object') {
-    //       removeEmpty(obj[key]);
-    //     } else if (obj[key] === '' || obj[key] === null) {
-    //       delete obj[key];
-    //     }
-    //     if (obj.anagrafica && Object.keys(obj.anagrafica).length === 0) {
-    //       delete obj.anagrafica;
-    //     }
-    //     if (obj.commessa && Object.keys(obj.commessa).length === 0) {
-    //       delete obj.commessa;
-    //     }
-    //     if (obj.contratto && Object.keys(obj.contratto).length === 0) {
-    //       delete obj.contratto;
-    //     }
-    //     if (obj.tipoContratto && Object.keys(obj.tipoContratto).length === 0) {
-    //       delete obj.tipoContratto;
-    //     }
-    //     if (obj.tipoAzienda && Object.keys(obj.tipoAzienda).length === 0) {
-    //       delete obj.tipoAzienda;
-    //     }
-    //     if (
-    //       obj.contrattoNazionale &&
-    //       Object.keys(obj.contrattoNazionale).length === 0
-    //     ) {
-    //       delete obj.contrattoNazionale;
-    //     }
-    //     if (
-    //       obj.livelloContratto &&
-    //       Object.keys(obj.livelloContratto).length === 0
-    //     ) {
-    //       delete obj.livelloContratto;
-    //     }
-    //     if (obj.ruolo && Object.keys(obj.ruolo).length === 0) {
-    //       delete obj.ruolo;
-    //     }
-    //   });
-    // };
+    // Ottieni i dati delle commesse dall'array 'commesse' nel form
+    const commesse = this.anagraficaDto.get('commesse')?.value;
 
-    // removeEmpty(this.anagraficaDto.value);
+    // Crea il payload con i dati delle commesse corretti
+    const payload = {
+      anagrafica: this.anagraficaDto.get('anagrafica')?.value,
+      commesse: commesse, // Usa i dati delle commesse estratti dal form
+      contratto: this.anagraficaDto.get('contratto')?.value,
+      ruolo: this.anagraficaDto.get('ruolo')?.value,
+    };
 
-    let check = true;
-
-    // if (this.anagraficaDto.value.anagrafica != null) {
-    //   if (
-    //     this.checkValid([
-    //       'anagrafica.nome',
-    //       'anagrafica.cognome',
-    //       'anagrafica.codiceFiscale',
-    //       'anagrafica.mailAziendale',
-    //     ])
-    //   ) {
-    //     return;
-    //   }
-    // } else {
-    //   return;
-    // }
-
-    // if (this.anagraficaDto.value.commessa != null) {
-    //   if (
-    //     this.checkValid([
-    //       'commesse.cliente',
-    //       'commesse.dataInizio',
-    //       'commesse.dataFine',
-    //       'commesse.nominativo',
-    //     ])
-    //   ) {
-    //     return;
-    //   }
-    // } else {
-    //   this.reset([
-    //     'commesse.cliente',
-    //     'commesse.dataInizio',
-    //     'commesse.dataFine',
-    //     'commesse.nominativo',
-    //   ]);
-    // }
-
-    // if (this.anagraficaDto.value.contratto != null) {
-    //   if (
-    //     (check = this.checkValid([
-    //       'contratto.tipoContratto.id',
-    //       'contratto.livelloContratto.id',
-    //       'contratto.contrattoNazionale.id',
-    //       'contratto.tipoAzienda.id',
-    //     ]))
-    //   ) {
-    //     return;
-    //   }
-    // } else {
-    //   this.reset([
-    //     'contratto.tipoContratto.id',
-    //     'contratto.livelloContratto.id',
-    //     'contratto.contrattoNazionale.id',
-    //     'contratto.tipoAzienda.id',
-    //   ]);
-    // }
-    const body = JSON.stringify({
-      anagraficaDto: this.anagraficaDto.value,
-    });
-    console.log(' BODY: ', body);
-
-    const commesseFormArray = this.anagraficaDto.get('commesse') as FormArray;
-    commesseFormArray.clear();
-
-    this.data?.commesse?.forEach((commessa: any) => {
-      commesseFormArray.push(this.formBuilder.group(commessa));
-    });
-    this.anagraficaDtoService.update(body).subscribe(
-      (response: any) => {
-        const result = response.esito;
-        console.log(result);
-
-        if (result.code === 0) {
-          alert('modifica riuscita');
-          if (result.anagraficaDto) {
-            this.anagraficaDto.patchValue(result.anagraficaDto);
-          }
-        } else {
-          alert('modifica non riuscita \n' + 'target: ' + result.target);
-          this.showErrorPopup = true;
-          this.errore = true;
-          this.messaggio = result.target;
-        }
+    console.log('payload backend:', payload);
+    this.anagraficaDtoService.update(payload).subscribe(
+      (response) => {
+        console.log('Payload inviato con successo al server:', response);
       },
-      (error: any) => {
-        console.error('Si è verificato un errore durante la modifica:', error);
-        // Gestisci l'errore in modo più utile per l'utente, ad esempio mostrando un messaggio di errore
+      (error) => {
+        console.error("Errore nell'invio del payload al server:", error);
       }
     );
   }
-
-  // this.anagraficaDtoService.update(body).subscribe(
-  //   (result) => {
-  //     console.log(result);
-  //     if ((result as any).esito.code != 0) {
-  //       alert(
-  //         'modifica non riuscita \n' +
-  //           'target: ' +
-  //           (result as any).esito.target
-  //       );
-  //       this.showErrorPopup = true;
-  //       this.errore = true;
-  //       this.messaggio = (result as any).esito.target;
-  //       return;
-  //     } else {
-  //       alert('modifica riuscita');
-  //       this.anagraficaDto = result;
-  //     }
-  //   },
-  //   (error: string) => {
-  //     console.error('Si é verificato un errore durante la modifica: ' + error);
-  //   }
-  // );
 
   transformDate(dateString: string): string {
     const dateObject = new Date(dateString);
@@ -393,40 +275,38 @@ export class ModificaAnagraficaDtoComponent implements OnInit {
     }
   }
 
+  caricaTipoContratto() {
+    this.anagraficaDtoService.getTipoContratto().subscribe((result: any) => {
+      this.tipiContratti = (result as any)['list'];
+    });
+  }
+  caricaLivelloContratto() {
+    this.anagraficaDtoService.getLivelloContratto().subscribe((result: any) => {
+      this.livelliContratti = (result as any)['list'];
+    });
+  }
+  caricaTipoAzienda() {
+    this.anagraficaDtoService.getTipoAzienda().subscribe((result: any) => {
+      this.tipiAziende = (result as any)['list'];
+    });
+  }
+
+  caricaContrattoNazionale() {
+    this.anagraficaDtoService
+      .getContrattoNazionale()
+      .subscribe((result: any) => {
+        this.ccnl = (result as any)['list'];
+      });
+  }
+  caricaListaUtenti() {
+    this.anagraficaDtoService.getListaUtenti().subscribe((result: any) => {
+      this.utenti = (result as any)['list'];
+    });
+  }
+
   caricaRuoli() {
     this.anagraficaDtoService.getRuoli().subscribe((result: any) => {
       this.ruoli = (result as any)['list'];
     });
-  }
-
-  aggiungiCommessa() {
-    const commessaFormGroup = this.creaFormCommessa();
-    this.commesse.push(commessaFormGroup);
-    // this.isFormDuplicated = true;
-    this.formsDuplicati.push(true);
-  }
-
-  creaFormCommessa(): FormGroup {
-    return this.formBuilder.group({
-      id: new FormControl(''),
-      cliente: new FormControl(''),
-      clienteFinale: new FormControl(''),
-      titoloPosizione: new FormControl(''),
-      distacco: new FormControl(''),
-      costoMese: new FormControl(''),
-      dataInizio: new FormControl(''),
-      dataFine: new FormControl(''),
-      tariffaGiornaliera: new FormControl(''),
-      nominativo: new FormControl(''),
-      azienda: new FormControl(''),
-      aziendaDiFatturazioneInterna: new FormControl(''),
-      stato: new FormControl(''),
-      attesaLavori: new FormControl(''),
-    });
-  }
-
-  rimuoviCommessa(index: number) {
-    this.commesse.removeAt(index);
-    this.formsDuplicati.splice(index);
   }
 }
