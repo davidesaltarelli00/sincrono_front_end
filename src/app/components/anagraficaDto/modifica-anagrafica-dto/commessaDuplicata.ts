2782bcce154c8e9a2c41e0603010196850b07e3e
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-09-08 13:03:34 +0200

    migliorato layout delle commesse su modifica anagrafica

--- src/app/components/anagraficaDto/modifica-anagrafica-dto/commessaDuplicata.ts
+++ src/app/components/anagraficaDto/modifica-anagrafica-dto/commessaDuplicata.ts
@@ -9,8 +9,6 @@ export class CommessaDuplicata {
   dataInizio: any;
   dataFine: any;
   tariffaGiornaliera: any;
-  nominativo: any;
-  azienda:any;
   aziendaDiFatturazioneInterna: any;
   attivo: any;
   attesaLavori: any;
