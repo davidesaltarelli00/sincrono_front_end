a7d12701dceb8a398374efbacf9d7a17d3802766
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-09-28 17:54:29 +0200

    storico contratti modificato

--- src/app/components/storici/storico-contratti/storico-contratti.component.ts
+++ src/app/components/storici/storico-contratti/storico-contratti.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StoricoService } from '../storico-service';
 import { ActivatedRoute} from '@angular/router';
+import { AlertDialogComponent } from 'src/app/alert-dialog/alert-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 declare var $: any;
 
@@ -24,7 +26,8 @@ export class StoricoContrattiComponent implements OnInit  {
     private storicoService: StoricoService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    public dialog:MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -32,16 +35,20 @@ export class StoricoContrattiComponent implements OnInit  {
     this.idAnagrafica = this.activatedRoute.snapshot.params['id'];
 //TO DO           DA CONTROLLARE
     this.storicoService.getStoricoContratti(this.idAnagrafica, localStorage.getItem("token")).subscribe((resp: any) => {
-      this.lista = resp.list;
 
-      console.log("lista contratti:"+ resp.list);
-
-      // $(function () {
-      //   $('#table').DataTable({
-      //     autoWidth: false,
-      //     responsive: true,
-      //   });
-      // });
+      if ((resp as any).esito.code !== 200) {
+        const dialogRef = this.dialog.open(AlertDialogComponent, {
+          data: {
+            title: 'Caricamento non riuscito:',
+            message: (resp as any).esito.target,
+          },
+        });
+      } else{
+        this.lista = resp.list;
+        console.log("lista contratti:"+ resp.list);
+      }
+    }, (error:any)=>{
+      console.error("Si e verificato un errore durante il caricamento dei dati:"+ error);
     });
   }
 
