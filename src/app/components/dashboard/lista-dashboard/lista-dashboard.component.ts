84e39fa6e39604c85461c5400eecb62b240b5dea
Ruamez <86289583+Ruamez@users.noreply.github.com>
2023-10-03 17:32:50 +0200

    03-10-2023 DashboardQuasiOperativa

--- src/app/components/dashboard/lista-dashboard/lista-dashboard.component.ts
+++ src/app/components/dashboard/lista-dashboard/lista-dashboard.component.ts
@@ -43,6 +43,7 @@ export class ListaDashboardComponent {
   listaContrattiInScadenza: any[] = []; //array 2.0
   listaCommesseScadute: any[] = []; //array 2.0
   listaContrattiFromBatch: any[] = []; //array 2.0
+  listaAnagraficheCommesseScadute: any[] = [];
   mostraFiltri = false;
   originalLista: any;
   tipiAziende: any = [];
@@ -191,22 +192,30 @@ export class ListaDashboardComponent {
           );
         }
       );
-    this.dashboardService
+   
+      this.dashboardService
       .getAllCommesseScadute(localStorage.getItem('token'))
       .subscribe(
         (resp: any) => {
-          for (const item of resp.list) {
+          const filteredList = resp.list.filter((item: any) => item.commesse.length > 0);
+          
+          filteredList.forEach((item: any) => {
             if (item.anagrafica && item.anagrafica.id) {
               this.idAnagraficaCommessaScaduta = item.anagrafica.id;
+              // Your logic for anagrafiche with commesse here
+              this.listaAnagraficheCommesseScadute.push(item.anagrafica);
+              console.log(this.listaAnagraficheCommesseScadute);
             }
-            for (const commesse of item.commesse) {
+            item.commesse.forEach((commesse: any) => {
               this.listaCommesseScadute.push(commesse);
-            }
-          }
+            });
+          });
+    
           console.log(
             'Lista commesse scadute: ' +
             JSON.stringify(this.listaCommesseScadute)
           );
+    
           this.currentPage = 1;
           this.pageData = this.getCurrentPageItems();
         },
@@ -217,7 +226,7 @@ export class ListaDashboardComponent {
           );
         }
       );
-
+    
     this.mostraFiltri = false;
   }
   filter(value: any) {
@@ -379,136 +388,21 @@ export class ListaDashboardComponent {
         this.router.navigate(['/dettaglio-anagrafica/' + idAnagrafica]);
       });
   }
+  vaiAModificaAnagrafica(idAnagrafica: number) {
+    this.anagraficaDtoService
+      .update(idAnagrafica, localStorage.getItem('token'))
+      .subscribe((resp: any) => {
+        console.log(resp);
+        this.router.navigate(['/modifica-anagrafica/' + idAnagrafica]);
+      });
+  }
+
 
   profile() {
     this.router.navigate(['/profile-box/', this.userlogged]);
   }
 
-  calcolaPeriodi(mesiDurata: any, descrizione: any): any {
-    if (descrizione == 'METALMECCANICO PMI CONFAPI') {
-      return mesiDurata / 3;
-    }
 
-    return mesiDurata / 2;
-  }
-
-  calcolaMesiPassati(dataAssunzione: Date, dataOdierna: Date): number {
-    if (!dataAssunzione || !dataOdierna) {
-      return 0;
-    }
-
-    const startYear = dataAssunzione.getFullYear();
-    const startMonth = dataAssunzione.getMonth() + 1;
-    const endYear = dataOdierna.getFullYear();
-    const endMonth = dataOdierna.getMonth() + 1;
-    return (endYear - startYear) * 12 + (endMonth - startMonth);
-  }
-
-  calcolaScadenzaImminentePeriodo(
-    dataAssunzione: Date,
-    mesiDurata: number,
-    descrizione: string
-  ) {
-    const currentDataAssunzione = new Date(dataAssunzione);
-    const dataOdierna = new Date();
-
-    if (descrizione == 'METALMECCANICO PMI CONFAPI') {
-      if (
-        this.calcolaMesiPassati(currentDataAssunzione, dataOdierna) ==
-        mesiDurata - 1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      if (
-        this.calcolaMesiPassati(currentDataAssunzione, dataOdierna) ==
-        mesiDurata - 1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
-  calcolaScadenzaImminentePeriodo2(
-    dataAssunzione: Date,
-    mesiDurata: number,
-    descrizione: string
-  ) {
-    const currentDataAssunzione = new Date(dataAssunzione);
-    const dataOdierna = new Date();
-
-    if (descrizione == 'METALMECCANICO PMI CONFAPI') {
-      if (
-        this.calcolaMesiPassati(currentDataAssunzione, dataOdierna) ==
-        mesiDurata * 2 - 1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      if (
-        this.calcolaMesiPassati(currentDataAssunzione, dataOdierna) ==
-        mesiDurata * 2 - 1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
-  calcolaScadenzaImminentePeriodo3(dataAssunzione: Date, mesiDurata: number) {
-    const currentDataAssunzione = new Date(dataAssunzione);
-    const dataOdierna = new Date();
-
-    if (
-      this.calcolaMesiPassati(currentDataAssunzione, dataOdierna) ==
-      mesiDurata * 3 - 1
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  scadenzaContratti40Giorni(dataAssunzione: any, mesiDurata: any) {
-    var currentDataAssunzione = new Date(dataAssunzione);
-    var fineContratto = new Date(dataAssunzione);
-    var year = currentDataAssunzione.getFullYear();
-    fineContratto.setFullYear(
-      year + Math.floor(mesiDurata / 12),
-      currentDataAssunzione.getMonth() + (mesiDurata % 12),
-      currentDataAssunzione.getDate()
-    );
-    var currentDate = new Date();
-
-    // Calculate the difference in days between the end date and current date
-    var daysRemaining = Math.ceil(
-      (fineContratto.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24)
-    );
-
-    // Determine if the remaining days are within the specified threshold
-    var thresholdDays = 40; // Adjust this value as per your requirement
-    return daysRemaining <= thresholdDays;
-  }
-
-  scadenzaCommesse40Giorni(dataFine: any) {
-    var dataAttuale = new Date();
-    var fineCommessa = new Date(dataFine);
-    // Calculate the difference in days between the end date and current date
-    var daysRemaining = Math.ceil(
-      (dataAttuale.getTime() - fineCommessa.getTime()) / (1000 * 60 * 60 * 24)
-    );
-
-    // Determine if the remaining days are within the specified threshold
-    var thresholdDays = 40; // Adjust this value as per your requirement
-    return daysRemaining <= thresholdDays;
-  }
 
   transformDate(dateString: string): string {
     const dateObject = new Date(dateString);
@@ -805,11 +699,6 @@ export class ListaDashboardComponent {
 
 
   }
-
-
-
-
-
 
   setForm() {
     const meseDataFine = this.filterAnagraficaDto.get(
