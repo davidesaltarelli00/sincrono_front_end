150a0de805dd0068a0e1b6bf7de28f281eeaf913
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-10-04 14:52:53 +0200

    migliorata modale info commesse

--- src/app/components/modal-info-commesse/modal-info-commesse.component.ts
+++ src/app/components/modal-info-commesse/modal-info-commesse.component.ts
@@ -3,6 +3,8 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AnagraficaDtoService } from '../anagraficaDto/anagraficaDto-service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-modal-info-commesse',
@@ -12,7 +14,7 @@ import { AnagraficaDtoService } from '../anagraficaDto/anagraficaDto-service';
 export class ModalInfoCommesseComponent implements OnInit {
   id: any = this.activatedRoute.snapshot.params['id'];
   mobile: boolean;
-  elencoCommesse: any[]=[];
+  elencoCommesse: any[] = [];
 
   constructor(
     private dialog: MatDialog,
@@ -20,8 +22,9 @@ export class ModalInfoCommesseComponent implements OnInit {
     private router: Router,
     private datePipe: DatePipe,
     private anagraficaDtoService: AnagraficaDtoService,
+    private clipboard: Clipboard,
+    private snackBar: MatSnackBar,
     @Inject(MAT_DIALOG_DATA) public data: any
-
   ) {
     if (window.innerWidth >= 900) {
       // 768px portrait
@@ -38,7 +41,19 @@ export class ModalInfoCommesseComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  copy(data: any) {
+    this.clipboard.copy(data);
+    this.snackBar.open(data + ' copiato negli appunti', '', {
+      duration: 3000,
+    });
+  }
+
+  modificaCommessa(id: any) {
+    console.log(id);
+    this.router.navigate(['/modifica-anagrafica/' + id]);
+    this.dialog.closeAll();
   }
 
   closeDialog(): void {
