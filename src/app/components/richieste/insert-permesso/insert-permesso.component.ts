b1ab6f8971645391c712563a5c4e1c7178cf87a7
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-11-24 15:12:19 +0100

    modificato il form della richiesta permessi

--- src/app/components/richieste/insert-permesso/insert-permesso.component.ts
+++ src/app/components/richieste/insert-permesso/insert-permesso.component.ts
@@ -51,6 +51,10 @@ export class InsertPermessoComponent implements OnInit {
   daOra: string = '';
   aOra: string = '';
 
+  arrayDaOra = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"];
+  arrayAOra = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"];
+
+
   constructor(
     private profileBoxService: ProfileBoxService,
     private dialog: MatDialog,
@@ -112,46 +116,58 @@ export class InsertPermessoComponent implements OnInit {
     console.log('Hai selezionato ' + this.permessoMese);
   }
 
-  insertPermesso() {
-    let body = {
-      richiestaDto: {
-        anno: this.permessoAnno,
-        mese: this.permessoMese,
-        codiceFiscale: this.codiceFiscale,
-        list: [
-          {
-            permessi: true,
-            ferie: null,
-            daOra: this.daOra,
-            aOra: this.aOra,
-            nGiorno: this.permessoGiorno,
-          },
-        ],
-      },
-    };
-    console.log('PAYLOAD INVIO PERMESSO: ' + JSON.stringify(body));
-    this.richiesteService
-      .inviaRichiesta(this.token, body)
-      .subscribe((result: any) => {
-        if ((result as any).esito.code != 200) {
-          const dialogRef = this.dialog.open(AlertDialogComponent, {
-            data: {
-              image: '../../../../assets/images/logo.jpeg',
-              title: 'Invio non riuscito:',
-              message: (result as any).esito.target,
+  insertPermesso(insertPermeission:any) {
+    console.log("Valore form:", JSON.stringify(insertPermeission));
+    if(insertPermeission.valid){
+      let body = {
+        richiestaDto: {
+          anno: this.permessoAnno,
+          mese: this.permessoMese,
+          codiceFiscale: this.codiceFiscale,
+          list: [
+            {
+              permessi: true,
+              ferie: null,
+              daOra: this.daOra,
+              aOra: this.aOra,
+              nGiorno: this.permessoGiorno,
             },
-          });
-        }
-        else{
-          const dialogRef = this.dialog.open(AlertDialogComponent, {
-            data: {
-              image: '../../../../assets/images/logo.jpeg',
-              title: 'IRichiesta inviata',
-              // message: "Troverai l'elenco delle tue richieste nel tuo profilo.",
-            },
-          });
-        }
+          ],
+        },
+      };
+      console.log('PAYLOAD INVIO PERMESSO: ' + JSON.stringify(body));
+      this.richiesteService
+        .inviaRichiesta(this.token, body)
+        .subscribe((result: any) => {
+          if ((result as any).esito.code != 200) {
+            const dialogRef = this.dialog.open(AlertDialogComponent, {
+              data: {
+                image: '../../../../assets/images/logo.jpeg',
+                title: 'Invio non riuscito:',
+                message: (result as any).esito.target,
+              },
+            });
+          }
+          else{
+            const dialogRef = this.dialog.open(AlertDialogComponent, {
+              data: {
+                image: '../../../../assets/images/logo.jpeg',
+                title: 'Richiesta inviata',
+                // message: "Troverai l'elenco delle tue richieste nel tuo profilo.",
+              },
+            });
+          }
+        });
+    } else{
+      const dialogRef = this.dialog.open(AlertDialogComponent, {
+        data: {
+          image: '../../../../assets/images/danger.png',
+          title: 'Attenzione',
+          message: "Qualcosa é andato storto.",
+        },
       });
+    }
+
   }
 
   logout() {
