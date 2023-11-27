1bc38eec797d37e827ed2b8ec95403a8c3b325e9
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-11-27 13:45:27 +0100

    no message

--- src/app/components/storici/storico-service.ts
+++ src/app/components/storici/storico-service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoricoService {
   token: any;
-
+  url = environment.URL_locale_Sincrono;
+  testUrl = environment.URL_login_service;
+  urlProd = environment.URL_PROD;
   constructor(private http: HttpClient) {}
 
   getStoricoContratti(id: any, token: any): Observable<any> {
@@ -34,14 +37,15 @@ export class StoricoService {
     );
   }
 
-  riattivaCommessa(body: any, token:any): Observable<any>{
+  riattivaCommessa(body: any, token: any): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       Authorization: `Bearer ${token}`,
     });
     return this.http.put<any>(
-      `http://localhost:8080/services/retain-commessa`,body,
+      `http://localhost:8080/services/retain-commessa`,
+      body,
       { headers: headers }
     );
   }
