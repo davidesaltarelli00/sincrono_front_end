4911d7576abf71399957c1c1177731f56632ee43
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-12-13 14:45:14 +0100

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   URL_locale_login: 'http://localhost:8080/login-service/',
   URL_login_service: `http://localhost:8085/`,
   URL_locale_Sincrono: 'http://localhost:8080/services/',
