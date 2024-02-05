afbcefd6035f6e29aa756a3b97b71d140357d943
Ruamez <ruamepnn@gmail.com>
2024-02-05 11:22:01 +0100

    AggiornatoEnvPerUsoLocaleTomcat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: false,
-  URL_locale_login: 'http://localhost:8080/login-service/',
+  URL_locale_login: 'http://localhost:8080/LoginService/login-service/',
   URL_login_service: `http://localhost:8085/`,
-  URL_locale_Sincrono: 'http://localhost:8080/services/',
+  URL_locale_Sincrono: 'http://localhost:8080/LoginService/services/',
   URL_PROD: 'http://192.168.58.196:8083/',
   URL_PRODUZIONE: 'http://192.168.58.196:8080/services/',
   URL_PRODUZIONEProva: 'http://192.168.58.196:8080/LoginService/services/',
   URL_PRODUZIONE_LOGIN: 'http://192.168.58.196:8080/LoginService/login-service/',
 };
+
+
+
